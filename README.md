d7a8dc2f4831925261ac511bb6e0851d22cf6fc0
kyberdrb <kyberdrb@gmail.com>
2017-08-22 12:21:27 +0200

    initial

---
+++ README.md
@@ -0,0 +1,12 @@
+# backup_home_directory
+
+Backs up the home directory of an user to an external drive.
+
+Check the script to find out more.
+
+# Sources
+https://stackoverflow.com/questions/4181703/how-to-concatenate-string-variables-in-bash  
+https://stackoverflow.com/questions/18568706/check-number-of-arguments-passed-to-a-bash-script  
+https://stackoverflow.com/questions/1378274/in-a-bash-script-how-can-i-exit-the-entire-script-if-a-certain-condition-occurs  
+https://unix.stackexchange.com/questions/65077/is-it-possible-to-see-cp-speed-and-percent-copied  
+https://unix.stackexchange.com/questions/12198/preserve-the-permissions-with-rsync  
---
+++ backup_user_profile.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# The first parameter is the path to the external storage.
+# It's the only and required parameter.
+
+if [ "$#" -ne 1 ]; then
+  echo "Illegal number of parameters"
+  echo "Usage:"
+  echo "$0 <path_to_external_drive>"
+  echo "e.g."
+  echo "$0 /run/media/andrej/3f090f89-297f-40ea-8a8e-86ae9dc3c2b4/"
+  exit 1
+fi
+
+echo
+echo "Let's start"
+echo
+echo "Set variables"
+echo
+
+BACKUP_STORAGE_PATH=$1
+PACMAN_INSTALLED_PACKAGES="pacman_installed_packages.txt"
+
+echo
+echo "Adjust owner of the external drive"
+echo
+
+#sudo chown -R $USER:users /run/media/andrej/3f090f89-297f-40ea-8a8e-86ae9dc3c2b4
+
+# Backup list of system packages
+echo "Backing up list of installed packages ..."
+pacman -Q > $BACKUP_STORAGE_PATH$PACMAN_INSTALLED_PACKAGES
+echo "Done!"
+
+# Backup files in user profile directory
+echo "Backing up user profile directory ..."
+su -c "time rsync -avhp --progress $HOME $BACKUP_STORAGE_PATH"
+echo "Done!"
+
