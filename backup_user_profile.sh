8f323b25d6d879405f6af253d48682f56a193cb1
kyberdrb <kyberdrb@gmail.com>
2017-11-27 07:21:25 +0100

    oversimplilification

--- backup_user_profile.sh
+++ backup_user_profile.sh
@@ -1,20 +1,6 @@
 
 #!/bin/bash
 
-# The first parameter is the path to the external storage.
-# It's the only and required parameter.
-
-if [ "$#" -ne 1 ]; then
-  echo "Illegal number of parameters"
-  echo "Usage:"
-  echo "$0 <path_to_external_drive>"
-  echo "e.g."
-  echo "$0 /run/media/andrej/3f090f89-297f-40ea-8a8e-86ae9dc3c2b4/"
-  exit 1
-fi
-
-echo
-
 echo "Let's start"
 
 echo
@@ -28,13 +14,26 @@ pacman -Qe | cut -d ' ' -f1 > \
 echo "The lsit of installed packages has been saved in \
 \"/home/andrej/github/Linux_tutorials/ARCH_installed_packages.txt\""
 
-echo "Backing up user profile directory ..."
+echo
+echo "************************"
+echo "CHECK DISK NAMES!"
+echo
 
-BACKUP_STORAGE_PATH=$1
+lsblk
 
-#su -c "time rsync -avhp --progress $HOME $BACKUP_STORAGE_PATH"
-#rsync nahradit dd prikazom
-dd if=/dev/sdX of=/dev/sdY bs=64K conv=noerror,sync status=progress
+echo "************************"
+echo
+echo "Cloning ..."
+echo
+echo From /dev/sda
+echo To   /dev/sdb
+echo
+echo "************************"
+echo "MAKE SURE, THE DISK NAMES ARE CORRECT AND NO OTHER REMOVABLE\
+ STORAGE DEVICE IS CONNECTED TO THE COMPUTER!"
+echo "************************"
+
+
+su -c 'time dd if=/dev/sda of=/dev/sdb bs=64K conv=noerror,sync status=progress'
 
 echo "Done!"
-
